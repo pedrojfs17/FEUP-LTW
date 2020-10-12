db6af387d3ec129a7de1dafeb4380b7f1f4cdff9
Pedro Seixas <49153615+pedrojfs17@users.noreply.github.com>
2020-10-12 18:51:17 +0100

    Started tp03

---
+++ TP03/HTTP Parameters/sum2.php
@@ -0,0 +1,16 @@
+<html>
+
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>FEUP - Em Constru&ccedil;&atilde;o</title>
+    </head>
+
+    <body>
+        <h1>Calculator</h1>
+        <h2><?php echo $_GET['num1'];?> + 
+            <?php echo $_GET['num2'];?> = 
+            <?php echo ($_GET['num1'] + $_GET['num2']); ?>
+        </h2>
+        <h3><a href="form2.html">Return</a></h3>
+    </body>
+</html>
