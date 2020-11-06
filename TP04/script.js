47b85123db833740d3b4939e84142e0859836ceb
Pedro Seixas <49153615+pedrojfs17@users.noreply.github.com>
2020-11-06 11:11:41 +0000

    Total

--- TP04/script.js
+++ TP04/script.js
@@ -2,7 +2,7 @@ let form = document.getElementsByTagName('form')[0]
 console.log(form.outerHTML);
 form.addEventListener('submit', function(event) {
     event.preventDefault();
-    console.log(event)
+
     const description = event.target[0].value;
     const quantity = event.target[1].value;
 
@@ -17,16 +17,22 @@ form.addEventListener('submit', function(event) {
     inputQuantity.setAttribute('value', quantity);
     td2.append(inputQuantity);
 
+    inputQuantity.addEventListener('change', function(event) {
+        inputQuantity.setAttribute('value', event.target.value);
+        updateTotal();
+    });
+
     let td3 = document.createElement('td');
     let inputRemove = document.createElement('input');
     inputRemove.setAttribute('type', 'submit');
     inputRemove.setAttribute('value', 'Remove');
     td3.append(inputRemove);
 
-    inputRemove.onclick = (event) => {
-        event.preventDefault()
-        tr.remove()
-      }
+    inputRemove.addEventListener('click', function(event) {
+        event.preventDefault();
+        tr.remove();
+        updateTotal();
+    });
 
     tr.append(td1);
     tr.append(td2);
@@ -34,12 +40,21 @@ form.addEventListener('submit', function(event) {
 
     document.querySelector('#products tbody').append(tr);
 
+    updateTotal();
+    
     alert('Submitted!');
 })
 
-let secondInput = document.querySelector('form label:nth-child(2) input');
-console.log(secondInput.outerHTML);
+function updateTotal() {
+    let tr = document.querySelectorAll('#products tbody tr');
+    let total = 0;
 
-let allInputs = document.querySelectorAll('form label input');
-for (let input of allInputs)
-    console.log(input.outerHTML);
+    tr.forEach((line) => {
+        if (line.children[0].tagName != 'TH') {
+            inputValue = line.children[1].children[0].attributes['value'].value;
+            total += parseInt(inputValue);
+        }
+    })
+
+    document.getElementById('total').innerHTML = total;
+}
