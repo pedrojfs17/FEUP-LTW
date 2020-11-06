b54f0b856273c4ddda8ba438665b151c171aed40
Pedro Seixas <49153615+pedrojfs17@users.noreply.github.com>
2020-11-06 10:54:26 +0000

    Add product

--- TP04/script.js
+++ TP04/script.js
@@ -2,6 +2,33 @@ let form = document.getElementsByTagName('form')[0]
 console.log(form.outerHTML);
 form.addEventListener('submit', function(event) {
     event.preventDefault();
+    console.log(event)
+    const description = event.target[0].value;
+    const quantity = event.target[1].value;
+
+    let tr = document.createElement('tr');
+
+    let td1 = document.createElement('td');
+    td1.innerHTML = description;
+
+    let td2 = document.createElement('td');
+    let inputQuantity = document.createElement('input');
+    inputQuantity.setAttribute('type', 'number');
+    inputQuantity.setAttribute('value', quantity);
+    td2.append(inputQuantity);
+
+    let td3 = document.createElement('td');
+    let inputRemove = document.createElement('input');
+    inputRemove.setAttribute('type', 'submit');
+    inputRemove.setAttribute('value', 'Remove');
+    td3.append(inputRemove);
+
+    tr.append(td1);
+    tr.append(td2);
+    tr.append(td3);
+
+    document.querySelector('#products tbody').append(tr);
+
     alert('Submitted!');
 })
 
